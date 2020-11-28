76115110a5e0b1714189c9a97795127b044d129c
Chris Michaud <cmichaud@utah.gov>
2020-11-28 15:02:53 -0700

    added upper white reach to reach()

--- R/reach.R
+++ R/reach.R
@@ -56,6 +56,8 @@ reach <- function(.data, RiverCode, rmi) {
                                      rmi < 71.7 ~ "LWH",
                                  RiverCode == "WH" &
                                      between(rmi, 71.7, 103.6) ~ "MWH",
+                                 RiverCode == "WH" &
+                                     rmi > 103.6 ~ "UWH",
                                  RiverCode == "DU" &
                                      rmi < 16.7 ~ "LDU",
                                  RiverCode == "DU" &
